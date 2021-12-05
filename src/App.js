55c8f09edb2e21b067f5d8650ca214c7867888d6
icarusdjr <icarus.djr@gmail.com>
2021-12-05 14:08:54 -0800

    reverse last edit

--- src/App.js
+++ src/App.js
@@ -39,14 +39,15 @@ const App = () => {
     
     let chainId = await ethereum.request({ method: 'eth_chainId' });
     console.log("Connected to chain " + chainId);
-    } else {
-        console.log("No authorized account found")
-    }
-
+    
     // String, hex code of the chainId of the Rinkebey test network
     const rinkebyChainId = "0x4"; 
     if (chainId !== rinkebyChainId) {
         alert("You are not connected to the Rinkeby Test Network!");
+    }
+
+    } else {
+        console.log("No authorized account found")
     }
 }
 
